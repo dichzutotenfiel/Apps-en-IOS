90e5b274b05bebf8385ff43bb5591c081448828f
Kevin Monge <kevinmonge@learniosfreecoms-mac.local>
2016-06-30 19:18:54 -0600

    Initial Commit

---
+++ pruebaaa/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  pruebaaa
+//
+//  Created by Kevin Monge on 6/30/16.
+//  Copyright © 2016 Kevin Monge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
